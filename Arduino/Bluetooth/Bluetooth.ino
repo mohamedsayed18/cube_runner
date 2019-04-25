0b48bf999cb9a2ea72f9e1bceb03103a10297750
Mohamed Ahmed <mohamed95.a.s@gmail.com>
2019-04-25 13:41:48 +0200

    BasicDemo update it is working on update instead of ienumerate

---
+++ Arduino/Bluetooth/Bluetooth.ino
@@ -0,0 +1,16 @@
+//Connections
+//bluetooth -----> Arduino
+//       TX -----> Pin 0 RX
+//  Rx      -----> Pin1 TX
+// VCC     ------> 5V
+// GND     ------> GND
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  Serial.println("wold");
+}
--- Assets/Scripts/change.cs
+++ Assets/Scripts/change.cs
@@ -1,35 +1,64 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
+using TechTweaking.Bluetooth; //include the library
 
 //change the color on command
-
 
 public class change : MonoBehaviour
 {
     //define some variables
+    private BluetoothDevice device; //creating an object(instance)
     Renderer thisRend; //renderer of our cube
-    float transitionTime = 5f; // Amount of time it takes to fade between colors
+    public Text statusText;
+    //float transitionTime = 5f; // Amount of time it takes to fade between colors
 
+    //this is called before the start
+    /* 
+    void Awake()
+    {
+        BluetoothAdapter.enableBluetooth();//Force Enabling Bluetooth
+        device = new BluetoothDevice(); //initialize the object with value
+        device.MacAddress = "20:16:03:07:13:26"; // the mac address of of the bluetooth
+        device.setEndByte(10);
+        device.ReadingCoroutine = null;
+        device.connect(); //will be in a separate function and called when a button is pressed
+    }
+    */
     // Start is called before the first frame update
     void Start()
     {
+        statusText.text = "Ready";
         thisRend = GetComponent<Renderer>(); // grab the renderer component on our sphere
-
     }
 
     // Update is called once per frame
+    /*
     void Update()
     {
-        
-        if (Input.GetKey("up"))//returns true if the key is pressed
-        {
-            print("up arrow key is held down");
-            thisRend.material.SetColor("_Color", Color.green);
+
+            if (device.IsDataAvailable)
+            {
+                byte[] msg = device.read();//because we called setEndByte(10)..read will always return a packet excluding the last byte 10.
+                if (msg != null && msg.Length > 0)
+                {
+                    string content = System.Text.ASCIIEncoding.ASCII.GetString(msg);
+                    if (content == "H")
+                    {
+                        statusText.text = "Reading= " + content;
+                        thisRend.material.SetColor("_Color", Color.green);
+                    }
+                    else
+                        thisRend.material.SetColor("_Color", Color.magenta);
+            }
         }
-        if (Input.GetKey("down"))//returns true if the key is pressed
-        {
-            print("down arrow key is held down");
+            else
+            {
+                thisRend.material.SetColor("_Color", Color.red);
+                statusText.text = "Nothing";
         }
     }
+    */
 }
